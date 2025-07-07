990a95d122588fe89e97d693c03efd83c93bf8e7
festradaScientone <137093258+festradaScientone@users.noreply.github.com>
2025-07-07 13:40:08 -0600

    update code

--- api/userinfo.js
+++ api/userinfo.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
   const auth = req.headers.authorization;
-  if (!auth) {
-    return res.status(401).json({ error: 'Missing Authorization header' });
-  }
+  if (!auth) return res.status(401).json({ error: 'Missing Authorization header' });
 
   try {
     const { data } = await axios.get(
